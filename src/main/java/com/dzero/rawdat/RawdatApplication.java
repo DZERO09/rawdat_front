13b5e6ca5b2f304012f894067736602808f910fe
B.Adam <belabessi.adam@gmail.com>
2022-08-15 23:12:43 +0100

    Initialize project using Create React App

---
+++ src/main/java/com/dzero/rawdat/RawdatApplication.java
@@ -0,0 +1,13 @@
+package com.dzero.rawdat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawdatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzero/rawdat/RawdatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzero.rawdat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
